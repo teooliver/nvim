f33ebc634f10456b9bbe21b92b693f4f97573132
Teo Oliver <teo.liz.oli@gmail.com>
2022-04-07 19:58:03 +0100

    chore: update keymaps and options

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -28,10 +28,15 @@ keymap("n", "<C-l>", "<C-w>l", opts)
 -- keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+-- keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+-- keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+-- keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+-- keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Horizontal Scroll
+-- Scroll left
+keymap("n", "<C-Right>", "zl", opts)
+keymap("n", "<C-Left>", "zh", opts)
 
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
